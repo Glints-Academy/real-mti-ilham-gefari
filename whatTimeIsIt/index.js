bdde893c812ba89eb9cf7d864ef8ec6c471a2bab
Sugianto <antho9922@gmal.com>
2021-01-27 20:59:01 +0800

    MTI sugianto

--- whatTimeIsIt/index.js
+++ whatTimeIsIt/index.js
@@ -1,11 +1,46 @@
 const whatTimeIsIt = time => {
   // do code here
+  if (time == '12:00 am') {
+    return "It's midnight";
+  } else if (time == '12:00 pm') {
+    return "It's noon";
+  } else {
+    let firstNumber = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    let secondNumber = ['','', 'twenty', 'thirty', 'forty', 'fifty']
 
+    let split1 = time.split(':');
+    let split2 = split1[1].split(' ');
+    let split3 = split2[0].split('');
+
+    const hour = firstNumber[split1[0]];
+
+    const minute = () => {
+      if (split2[0] < 20) {
+        return `${firstNumber[split2[0]]}`;
+      } else {
+        if (time == 20 || time == 30 || time == 40 || time == 50) {
+          return secondNumber[split3[0]];
+        } else {
+          return `${secondNumber[split3[0]]} ${firstNumber[split3[1]]}`;
+        }
+      }
+    }
+
+    const amPM = () => {
+      if (split2[1] == 'am') {
+        return 'in the morning';
+      } else {
+        return 'in the afternoon';
+      }
+    }
+
+    return `It's ${hour} past ${minute()} ${amPM()}`
+  }
 }
 
 
 // do not change this code below
-const test = (testCase, result) => console.log(testCase === result);
+const test = (testCase, result) => console.log(testCase === result, testCase);
 
 test(whatTimeIsIt("12:00 pm"), "It's noon")
 test(whatTimeIsIt("3:49 pm"), "It's three past forty nine in the afternoon")
